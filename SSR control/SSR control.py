497450fd5254de73ef20df70b3dfc74a8703b997
DF-Master <jiangyida@pku.edu.cn>
2021-04-09 12:48:10 +0800

    finish SSR control

--- SSR control/SSR control.py
+++ SSR control/SSR control.py
@@ -1,38 +1,37 @@
 import serial
 import time
-import binascii
 import streamlit as st
-# import sys
-# from PyQt5 import QtWidgets
-# from PyQt5.QtWidgets import *
-# from PyQt5.QtCore import *
-# from PyQt5.QtGui import *
-# import UI210331_1 as ui
+import csv
 
 
+
+        
 # 定义默认系统变量
 
+
+
 seropen = False
-openorder = 'A0 01 01 A2'
-closeorder = 'A0 01 00 A1'
+openorder = 'A0 01 00 A1'
+closeorder = 'A0 01 01 A2'
 openorder16bit = bytes.fromhex(openorder)
 closeorder16bit = bytes.fromhex(closeorder)
 startprogram = False
 
 # 定义可调变量
+global comdefault
+comdefault = 'COM3'
+defaultopentime = float(0.5)  # 这是开机时的测试时间
 
-defaultopentime = float(2)  # 这是开机启动时的预热测试时间
-opentimedefault = float(5)
-stoptimedefault = float(5)
-loopdefault = float(5)
-comdefault = 'COM4'
-waittimedefault = 5
 
+#
 # 打开串口
 ser = serial.Serial()
 
 
+
+
 def initSerial(comset = comdefault):
+
     global ser,seropen
     ser.baudrate = 9600
     #ser.port = '/dev/ttyUSB0'
@@ -43,32 +42,34 @@ def initSerial(comset = comdefault):
     ser.parity = serial.PARITY_NONE
     ser.rtscts = 0
     seropen=True
+    ser.open()  
 
 # 定义开关函数
-def TryOpen(comset):
-    try:    
-        global comdefault,breakloop
-        initSerial(comset)
-        ser.open()
-        seropen = True
-
-        Open()
-        time.sleep(defaultopentime)
-        Close()
+# def TryOpen(comset=comdefault):
+#     try:    
+#         global comdefault,breakloop
+#         # initSerial(comset)
         
-        comdefault = comset
-    except:
-        print('not ' + str(comset))
+#         Open()
+#         time.sleep(defaultopentime)
+#         Close()  
+#     except:
+#         print('not ' + str(comset))
+#     else:
+#         seropen = True
 
+
+#         comdefault = comset
+#         print('serial open, serial name is '+ comdefault)
+#         return True
 def Open():
-    for i in ['COM1','COM2','COM3','COM4','COM5','COM6','COM7','COM8','COM9','COM10']:
-        TryOpen(i)
-    print(comdefault)
     global seropen
     if seropen == False:
         print('open fail')
     else:
         ser.write(openorder16bit)
+        print('open')
+        seropen=False
 
 
 def Close():
@@ -77,89 +78,91 @@ def Close():
         print('open fail')
     else:
         ser.write(closeorder16bit)
+        print('close')
+        seropen=False
         
 
-
-# # 产生主界面
-# class ThreadRunLoop(QThread):
-#     sinOut = pyqtSignal(str)
-
-#     def ___init__(self):
-#         super(Thread, self).__init__()
-
-#     def run(self):
-#         # 线程相关的代码
-#         global opentimedefault, closetimedefault, loopdefault, comdefault 
-#         while True:
-#             if startprogram == True and waitinglist == 2:
-#                 DetectSet()
-#                 self.sinOut.emit(str(datarev))
-#                 waitinglist = 1  # 下一步为读取实时温度
-#                 # 将结果保存进csv
-#                 WriteLine(str(temprev), str(datarev), NowTime())  # 将结果保存入CSV的最后一行 
-
-#             sleep(waittime)
-
-# class Main(QMainWindow, ui.Ui_MainWindow):
-#     def __init__(self):
-#         # 初始化
-#         super().__init__()
-#         self.setWindowTitle('UV Controller --UI')
-#         self.setupUi(self)  # UI文件在先前的import已有定义
-
-#         # 打开多线程，利用多线程读取实时测得的温度与实时的温度设置（在实验中，设定温度一般为常数，考虑到设定温度可能是渐变函数的存在，这里保留实时更新功能）
-#         # self.threadtemprev = ThreadTempRev()
-#         # self.threadtemprev.start()
+            
         
-
-#         # 信号与槽函数的连接
-
-#         # self.opentime.sinOut.connect(self.OpenTimeChange)
-#         # self.closetime.sinOut.connect(self.CloseTimeChange)
-#         # 按钮与函数进行关联
-
-#         self.pbstop.clicked.connect(self.StopProgram)
-#         self.pbquit.clicked.connect(self.EndProgram)
-#         self.pbstart.clicked.connect(self.StartProgram)
-
-#     def EndProgram(self):
-#         sys.exit(app.exec_())
-
-#     def StartProgram(self):
-#         startprogram = True
-
-#     def StopProgram(self):
-#         startprogram = False
-
-#     def OpenTimeChange(self, str):
-#         if str.isdigit():
-#             global opentimedefault
-#             opentimedefault = float(str)
-`-+`
-#     def CloseTimeChange(self, str):
-#         if str.isdigit():
-#             global closetimedefault
-#             closetimedefault = float(str)
-
-
+        
 if __name__ == "__main__":
-    # for i in ['COM1','COM2','COM3','COM4','COM5','COM6','COM7','COM8','COM9','COM10']:
-    #     TryOpen(i)
-    # print(comdefault)
+    # if TryOpen(comdefault)==True:
+    #     print(comdefault)
+    # else:
+    #     for i in ['COM1','COM2','COM3','COM4','COM5','COM6','COM7','COM8','COM9','COM10']:
+    #         TryOpen(i)
+    #     print(comdefault)
+    # print('COM set')
+
+    
     st.title("UV网页控制UI")
     st.markdown('made by [Yida](https://github.com/DF-Master) --210402 update!',unsafe_allow_html=True)
+    comset = st.text_input('COMSet',value='COM4')
+    comdefault = str(comset)
     if st.button('Open'):
-        Open()
-        time.sleep(2)
+        try:
+            initSerial(comdefault)
+            Open()
+            ser.close()
+            st.markdown('open')
+        except:
+            st.markdown('open failed')
+            
     if st.button('Close'):
-        Close()
-
-
+        try:
+            initSerial(comdefault)
+            Close()
+            ser.close()
+            st.markdown('close')
+        except:
+            st.markdown('close failed')
         
+    if st.button('Search COM'):
+        for i in ['COM1','COM2','COM3','COM4','COM5','COM6','COM7','COM8','COM9','COM10']:
+            
+            try:
+                initSerial(i)
+                Open()
+                ser.close()
+            except:
+                st.markdown('not '+ i)
+            else:
+                st.markdown('Please set serial as '+ i)
+                comdefault = i
+                
+    if st.button('COM Now'):
+        st.markdown(str(comdefault))
 
-    # 产生UI
-    # # app = QtWidgets.QApplication(sys.argv)
-    # # window = Main()
-    # window.show()
-
-    # sys.exit(app.exec_())
+    
+    opentimelist = st.text_input('Open Time List',value='2,1')
+    closetimelist = st.text_input('Close Time List',value='2,1')
+    looptimelist = st.text_input('Loop Time List',value='1,1')
+    
+    if st.button('Run Program'):
+        try:
+            initSerial(comdefault)
+            Open()
+            ser.close()
+            initSerial(comdefault)
+            Close()
+            ser.close()
+        except:
+            st.markdown('open failed')
+        else:
+            st.markdown('test complete, run program')
+            ol = opentimelist.split(',')
+            cl = closetimelist.split(',')
+            ll = looptimelist.split(',')
+            for i in list(range(len(ol))):
+                for j in list(range(int(ll[i]))):
+                    initSerial(comdefault)
+                    Open()
+                    ser.close()
+                    time.sleep(float(ol[i]))
+                    initSerial(comdefault)
+                    Close()
+                    time.sleep(float(cl[i]))
+                    ser.close()
+                    st.markdown('Open '+ str(ol[i]) + '  Close ' + str(cl[i])+ '  Loop ' + str(j))
+            st.markdown('Program finish')
+    
--- SSR control/test.py
+++ SSR control/test.py
@@ -1,6 +1,1 @@
-import streamlit as st
-
-if st.button('Say hello'):
-    print('hello')
-else:
-    st.write('Goodbye')
+print(list(range(5)))
