9d0f74cc0a028b6c1f9c1af04a9293e676fe5a00
DF-Master <jiangyida@pku.edu.cn>
2021-06-05 20:59:46 +0800

    Update auto_photo_collect.sh

--- berry/auto_photo_collect.sh
+++ berry/auto_photo_collect.sh
@@ -1,10 +1,8 @@
 echo "start photo atuo collect"
 while true
 do
-    set FILENAME (date +"%Y%m%d%H%M%S") 
-    # only in fish shell
+    FILENAME=$(date +"%Y%m%d%H%M%S") 
     fswebcam /dev/video0 --no-banner -r 1080x720 /root/logicphotos/$FILENAME.jpg
     scp /root/logicphotos/$FILENAME.jpg root@badapple.online:/root/yidanetdisk
-
-    
+    sleep 5    
 done
