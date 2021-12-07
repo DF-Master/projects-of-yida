1c20edfc58c0e5641e44cc17589871322dfeb722
Yida Jiang <69508342+DF-Master@users.noreply.github.com>
2021-12-07 15:22:25 +0800

    1207

--- AI/AI_in_chem/YGO-AI/cdbopen/opencdb.py
+++ AI/AI_in_chem/YGO-AI/cdbopen/opencdb.py
@@ -1,7 +1,60 @@
-import cdblib
-with open('AI/AI_in_chem/YGO-AI/cdbopen/cards.cdb','rb') as f:
-    data=f.read()
-reader = cdblib.Reader(data)
-print(reader,type(reader),reader.iteritems())
-for key, value in reader.iteritems():
-    print('+{},{}:{}->{}'.format(len(key), len(value), key, value))
+import pydeck
+
+# Connenct cards.cdb
+con = pydeck.loadDatabase('AI/AI_in_chem/YGO-AI/cdbopen/cards_cn.cdb')
+
+# Find Card ID
+card_name = pydeck.getCardsFromName('连接栗子球')
+print('Card ID is: ', card_name)
+
+
+#
+def Search_all(CardID):
+    # Get Text with ID
+    get_text = pydeck.getText(CardID, 'name')
+    print('Card Text is:', get_text)
+
+    get_text = pydeck.getText(CardID, 'desc')
+    print('Card Function is:', get_text)
+
+    get_text = pydeck.getText(CardID, 'str1')
+    print('Card Tag1 is:', get_text)
+
+    get_text = pydeck.getText(CardID, 'str2')
+    print('Card Tag2 is:', get_text)
+
+    # Get Data with ID
+
+    get_text = pydeck.getData(CardID, 'race')
+    print('Card Race is:', get_text)
+
+    get_text = pydeck.getData(CardID, 'level')
+    print('Card level is:', get_text)
+
+    get_text = pydeck.getData(CardID, 'atk')
+    print('Card Atk is:', get_text)
+
+    get_text = pydeck.getData(CardID, 'def')
+    print('Card Def is:', get_text)
+
+    get_text = pydeck.getData(CardID, 'type')
+    print('Card Type is:', get_text)
+
+    get_text = pydeck.getData(CardID, 'ot')
+    print('Card Ot is:', get_text)
+
+    get_text = pydeck.getData(CardID, 'alias')
+    print('Card Alias is:', get_text)
+
+    get_text = pydeck.getData(CardID, 'setcode')
+    print('Card Setcode is:', get_text)
+
+    get_text = pydeck.getData(CardID, 'attribute')
+    print('Card Attribute is:', get_text)
+
+    get_text = pydeck.getData(CardID, 'category')
+    print('Card Category is:', get_text)
+
+
+if __name__ == '__main__':
+    Search_all(41999284)
