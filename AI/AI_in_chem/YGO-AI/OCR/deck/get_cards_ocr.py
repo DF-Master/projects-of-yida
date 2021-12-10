8642addbd94d315c2b28a9faf5ef5fb22c77416b
Yida Jiang <69508342+DF-Master@users.noreply.github.com>
2021-12-10 20:14:22 +0800

    1210

---
+++ AI/AI_in_chem/YGO-AI/OCR/deck/get_cards_ocr.py
@@ -0,0 +1,47 @@
+import easyocr
+from PIL import Image
+import numpy as np
+import cv2
+import csv
+
+
+def ocr_cards(card_name, writer, blur_x=3, blur_y=3, blur_sigma=0):
+    img = Image.open('./AI/AI_in_chem/YGO-AI/OCR/deck/deck_kaiba/' +
+                     card_name + ".jpg").resize((59 * 3, 86 * 3))
+    img = cv2.GaussianBlur(np.array(img), (blur_x, blur_y), blur_sigma)
+    reader = easyocr.Reader(['ch_sim', 'en'])
+    result = reader.readtext(img)
+    # if len(result) > 0:
+    #     if len(result[0]) > 1:
+
+    writer.writerow(
+        [result[0][1], result[0][2], card_name, blur_x, blur_y, blur_sigma])
+    print("Result:", result[0][1], result[0][2], card_name, blur_x, blur_y,
+          blur_sigma)
+
+
+with open('./AI/AI_in_chem/YGO-AI/OCR/deck/ocr_data.csv', "w") as f:
+    all_name = []
+    writer = csv.writer(f)
+    with open('./AI/AI_in_chem/YGO-AI/OCR/deck/all_cards.txt') as deck:
+        for card_id in deck:
+            if len(card_id.strip()) > 1:
+                try:
+                    card_id = str(int(card_id))
+                except:
+                    continue
+                else:
+                    if card_id in all_name:
+                        continue
+                    else:
+                        all_name.append(card_id)
+
+                        card_name = card_id
+                        for blur_sigma in range(20):
+                            for blur_x in [1, 3]:
+                                for blur_y in [1, 3]:
+                                    ocr_cards(card_name,
+                                              writer,
+                                              blur_x=blur_x,
+                                              blur_y=blur_y,
+                                              blur_sigma=blur_sigma / 10)
--- AI/AI_in_chem/YGO-AI/OCR/ocr_test.py
+++ AI/AI_in_chem/YGO-AI/OCR/ocr_test.py
@@ -1,8 +1,16 @@
 import easyocr
+from PIL import Image
+import numpy as np
+import cv2
 
+img = Image.open(
+    './AI/AI_in_chem/YGO-AI/OCR/deck/deck_kaiba/2129638.jpg').resize(
+        (59 * 3, 86 * 3))
+img = cv2.GaussianBlur(np.array(img), (3, 3), 0)
+cv2.imwrite('./AI/AI_in_chem/YGO-AI/OCR/deck/img_test.jpg', img)
 reader = easyocr.Reader(
     ['ch_sim',
      'en'])  # this needs to run only once to load the model into memory
-result = reader.readtext('./AI/AI_in_chem/YGO-AI/OCR/pics_en/32864.jpg')
-
-print("Result:", result)
+# result = reader.readtext(np.array(img))
+result = reader.readtext('./AI/AI_in_chem/YGO-AI/OCR/deck/img_test.jpg')
+print("Result:", result[0][1], result[0][2])
