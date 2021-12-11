3f318f1c61fdf8a6da00f4aceef2b0829e7d95b2
Yida Jiang <69508342+DF-Master@users.noreply.github.com>
2021-12-11 09:35:15 +0800

    1211

--- AI/AI_in_chem/YGO-AI/OCR/deck/get_cards_ocr.py
+++ AI/AI_in_chem/YGO-AI/OCR/deck/get_cards_ocr.py
@@ -5,41 +5,49 @@ import cv2
 import csv
 
 
-def ocr_cards(card_name, writer, blur_x=3, blur_y=3, blur_sigma=0):
-    img = Image.open('./AI/AI_in_chem/YGO-AI/OCR/deck/deck_kaiba/' +
-                     card_name + ".jpg").resize((59 * 3, 86 * 3))
+def ocr_cards(card_name, target, blur_x=3, blur_y=3, blur_sigma=0):
+    try:
+        img = Image.open('./AI/AI_in_chem/YGO-AI/OCR/deck/deck_kaiba/' +
+                         card_name + ".jpg").resize((59 * 3, 86 * 3))
+    except:
+        img = Image.open('./AI/AI_in_chem/YGO-AI/OCR/deck/deck_yogi/' +
+                         card_name + ".jpg").resize((59 * 3, 86 * 3))
+
     img = cv2.GaussianBlur(np.array(img), (blur_x, blur_y), blur_sigma)
     reader = easyocr.Reader(['ch_sim', 'en'])
     result = reader.readtext(img)
-    # if len(result) > 0:
-    #     if len(result[0]) > 1:
+    if len(result) > 0:
+        if len(result[0]) > 1:
+            target.writerow([
+                result[0][1], result[0][2], card_name, blur_x, blur_y,
+                blur_sigma
+            ])
+            print("Result:", result[0][1], result[0][2], card_name, blur_x,
+                  blur_y, blur_sigma)
 
-    writer.writerow(
-        [result[0][1], result[0][2], card_name, blur_x, blur_y, blur_sigma])
-    print("Result:", result[0][1], result[0][2], card_name, blur_x, blur_y,
-          blur_sigma)
 
-
-with open('./AI/AI_in_chem/YGO-AI/OCR/deck/ocr_data.csv', "w") as f:
+with open('./AI/AI_in_chem/YGO-AI/OCR/deck/all_cards.txt', 'r') as deck:
     all_name = []
-    writer = csv.writer(f)
-    with open('./AI/AI_in_chem/YGO-AI/OCR/deck/all_cards.txt') as deck:
-        for card_id in deck:
-            if len(card_id.strip()) > 1:
-                try:
-                    card_id = str(int(card_id))
-                except:
+    for card_id in deck:
+        if len(card_id.strip()) > 1:
+            try:
+                card_id = str(int(card_id))
+            except:
+                continue
+            else:
+                if card_id in all_name:
                     continue
                 else:
-                    if card_id in all_name:
-                        continue
-                    else:
-                        all_name.append(card_id)
+                    all_name.append(card_id)
 
-                        card_name = card_id
-                        for blur_sigma in range(20):
-                            for blur_x in [1, 3]:
-                                for blur_y in [1, 3]:
+                    card_name = card_id
+                    for blur_sigma in range(20):
+                        for blur_x in [1, 3]:
+                            for blur_y in [1, 3]:
+                                with open(
+                                        './AI/AI_in_chem/YGO-AI/OCR/deck/ocr_data.csv',
+                                        "a") as file:
+                                    writer = csv.writer(file)
                                     ocr_cards(card_name,
                                               writer,
                                               blur_x=blur_x,
---
+++ AI/AI_in_chem/YGO-AI/OCR/deck/test.py
@@ -0,0 +1,6 @@
+import csv
+with open('./AI/AI_in_chem/YGO-AI/OCR/deck/ocr_data.csv', "w") as f:
+
+    all_name = []
+    writer = csv.writer(f)
+    writer.writerows(['test'])
