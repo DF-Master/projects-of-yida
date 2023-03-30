d3e52a9a74151548768111097bc46f8452a51cb0
Yida Jiang <69508342+DF-Master@users.noreply.github.com>
2023-03-30 13:50:36 +0800

    1

--- AI/AI_in_chem/YGO-AI/OCR/ocr_test.py
+++ AI/AI_in_chem/YGO-AI/OCR/ocr_test.py
@@ -5,11 +5,11 @@ import cv2
 
 # img = Image.open('C:/Users/jiang/Downloads/QQ图片20211213192419.jpg').resize(
 # (59 * 3, 86 * 3))
-img = Image.open('C:/Users/jiang/Downloads/1.png')
+img = Image.open('C:/Users/jiang/Downloads/1.jpg')
 # img = cv2.GaussianBlur(np.array(img), (3, 3), 0)
 # cv2.imwrite('./AI/AI_in_chem/YGO-AI/OCR/deck/img_test.jpg', img)
 reader = easyocr.Reader(
-    ['ch_sim'])  # this needs to run only once to load the model into memory
+    ['en'])  # this needs to run only once to load the model into memory
 result = reader.readtext(np.array(img), detail=0)
 # result = reader.readtext('./AI/AI_in_chem/YGO-AI/OCR/deck/img_test.jpg')
 print("Result:", result)
